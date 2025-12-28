09a170235e75b6c695c3f671b7180b06e3cbdb2b
Kippess <110441194+Kippess@users.noreply.github.com>
2025-12-28 14:04:25 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
-const TOTAL_MATCHES = 1;
+const TOTAL_MATCHES = 0;
 
 const PLAYERS = {
   "Kippes": { matches: 0, mvp: 0 },
-  "Turro": { matches: 0, mvp: 0 },
+  "Alex": { matches: 0, mvp: 0 },
   "Demencia": { matches: 0, mvp: 0 },
   "Topa": { matches: 0, mvp: 0 },
   "Isleño": { matches: 0, mvp: 0 },
   "Faculo Airbag": { matches: 0, mvp: 0 },
-  "Fran": { matches: 0, mvp: 0 },
+  "Fran Oti": { matches: 0, mvp: 0 },
   "Ponchi": { matches: 0, mvp: 0 },
   "Jony sucio": { matches: 0, mvp: 0 },
   "Julio Metal": { matches: 0, mvp: 0 },
