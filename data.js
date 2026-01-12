e007359668a7b066f3de6df25fa41d1794466945
Kippess <110441194+Kippess@users.noreply.github.com>
2026-01-12 00:36:58 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,28 @@
 // Cantidad de domingos que se han jugado en total en la temporada
-const TOTAL_MATCHES = 1;
+const TOTAL_MATCHES = 2;
 
 const PLAYERS = {
-  "Kippes": { matches: 1, mvp: 0, foto: "kippes.jpg" },
+  "Kippes": { matches: 2, mvp: 0, foto: "kippes.jpg" },
   "Turro": { matches: 0, mvp: 0, foto: "Turro.jpg" },
   "Demencia": { matches: 0, mvp: 0, foto: "Demencia.jpg" },
-  "Topa": { matches: 1, mvp: 0, foto: "Topa.jpg" },
+  "Topa": { matches: 2, mvp: 0, foto: "Topa.jpg" },
   "Isleño": { matches: 1, mvp: 0, foto: "Isleno.jpg" },
   "Faculo Airbag": { matches: 1, mvp: 0, foto: "Faculo.jpg" },
-  "Fran": { matches: 1, mvp: 1, foto: "Fran.jpg" },
+  "Fran": { matches: 2, mvp: 1, foto: "Fran.jpg" },
   "Ponchi": { matches: 0, mvp: 0, foto: "Ponchi.jpg" },
   "Jony sucio": { matches: 0, mvp: 0, foto: "Jony.jpg" },
-  "Julio Metal": { matches: 1, mvp: 0, foto: "Julio.jpg" },
+  "Julio Metal": { matches: 2, mvp: 0, foto: "Julio.jpg" },
   "Valencia": { matches: 0, mvp: 0, foto: "Valencia.jpg" },
-  "Dinho": { matches: 1, mvp: 0, foto: "Dinho.jpg" },
+  "Dinho": { matches: 2, mvp: 0, foto: "Dinho.jpg" },
   "Cinema": { matches: 1, mvp: 0, foto: "cinema.jpg" },
-  "Chinche": { matches: 1, mvp: 0, foto: "Chinche.jpg" },
+  "Chinche": { matches: 2, mvp: 0, foto: "Chinche.jpg" },
   "Culebra": { matches: 0, mvp: 0, foto: "Culebra.jpg" },
   "Vegui": { matches: 0, mvp: 0, foto: "Vegui.jpg" },
-  "Mosky": { matches: 0, mvp: 0, foto: "Mosky.jpg" },
-  "Bylu": { matches: 1, mvp: 0, foto: "Bylu.jpg" },
-  "Pana Hija": { matches: 1, mvp: 0, foto: "Pana.jpg" }
+  "Mosky": { matches: 1, mvp: 0, foto: "Mosky.jpg" },
+  "Bylu": { matches: 2, mvp: 0, foto: "Bylu.jpg" },
+  "Pana Hija": { matches: 2, mvp: 0, foto: "Pana.jpg" }
+  "DJ Manzon": { matches: 1, mvp: 0, foto: "manzon.jpg" }
+  "Nuno": { matches: 1, mvp: 0, foto: "nuno.jpg" }
 };
 
 // Aquí irás agregando los partidos. 
@@ -29,4 +31,5 @@ const MATCH_HISTORY = [
   // Ejemplo de cómo anotar cuando jueguen:
   // { fecha: "05 Ene", resultado: "Verde 5 - 2 Azul", mvp: "Kippes" }
   { fecha: "05 Ene", resultado: "Pechera 3 - 0 Sin pechera", mvp: "Fran" }
+  { fecha: "12 Ene", resultado: "Pechera 8 - 12 Sin pechera", mvp: "?" }
 ];
