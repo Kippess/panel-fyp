cd4864c8c2e2b23e766f61d877bb5efe4032e6b5
Kippess <110441194+Kippess@users.noreply.github.com>
2026-03-23 18:39:34 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,34 @@
 // Cantidad de domingos jugados en total
-const TOTAL_MATCHES = 10;
+const TOTAL_MATCHES = 11;
 
 // Plata recaudada
 const FONDO_COMUN = 41170; 
 
 const PLAYERS = {
   // Se agregó "goalFech" a todos para que el sistema de puntos funcione correctamente.
-  "Kippes": { matches: 9, mvp: 2, wins: 2, goalFech: 0, foto: "kippes.jpg", bonus: 40 },
-  "Turro": { matches: 5, mvp: 0, wins: 0, goalFech: 0, foto: "Turro.jpg", bonus: 0 },
-  "Demencia": { matches: 1, mvp: 0, wins: 0, goalFech: 0, foto: "Demencia.jpg", bonus: 0 },
-  "Topa": { matches: 8, mvp: 0, wins: 2, goalFech: 0, foto: "Topa.jpg", bonus: 0 },
+  "Kippes": { matches: 10, mvp: 2, wins: 3, goalFech: 0, foto: "kippes.jpg", bonus: 40 },
+  "Turro": { matches: 6, mvp: 0, wins: 0, goalFech: 0, foto: "Turro.jpg", bonus: 0 },
+  "Demencia": { matches: 2, mvp: 0, wins: 0, goalFech: 0, foto: "Demencia.jpg", bonus: 0 },
+  "Topa": { matches: 9, mvp: 0, wins: 3, goalFech: 0, foto: "Topa.jpg", bonus: 0 },
   "Isleño": { matches: 1, mvp: 0, wins: 0, goalFech: 0, foto: "Isleno.jpg", bonus: 0 },
   "Faculo Airbag": { matches: 4, mvp: 0, wins: 2, goalFech: 0, foto: "Faculo.jpg", bonus: 0 },
-  "Fran": { matches: 9, mvp: 1, wins: 2, goalFech: 0, foto: "Fran.jpg", bonus: 20 },
+  "Fran": { matches: 10, mvp: 1, wins: 3, goalFech: 0, foto: "Fran.jpg", bonus: 20 },
   "Ponchi": { matches: 0, mvp: 0, wins: 0, goalFech: 0, foto: "Ponchi.jpg", bonus: 0 },
-  "Jony sucio": { matches: 1, mvp: 0, wins: 1, goalFech: 0, foto: "Jony.jpg", bonus: 0 },
-  "Julio Metal": { matches: 4, mvp: 1, wins: 1, goalFech: 0, foto: "Julio.jpg", bonus: 20 },
+  "Jony sucio": { matches: 2, mvp: 0, wins: 1, goalFech: 0, foto: "Jony.jpg", bonus: 0 },
+  "Julio Metal": { matches: 5, mvp: 1, wins: 1, goalFech: 0, foto: "Julio.jpg", bonus: 20 },
   "Valencia": { matches: 2, mvp: 0, wins: 1, goalFech: 0, foto: "Valencia.jpg", bonus: 0 },
-  "Bini": { matches: 4, mvp: 0, wins: 1, goalFech: 0, foto: "Bini.jpg", bonus: 0 },
+  "Bini": { matches: 5, mvp: 0, wins: 1, goalFech: 0, foto: "Bini.jpg", bonus: 0 },
   "Cinema": { matches: 3, mvp: 0, wins: 0, goalFech: 0, foto: "cinema.jpg", bonus: 0 },
-  "Chinche": { matches: 9, mvp: 1, wins: 3, goalFech: 1, foto: "Chinche.jpg", bonus: 0 },
-  "Culebra": { matches: 3, mvp: 1, wins: 1, goalFech: 1, foto: "Culebra.jpg", bonus: 0 },
-  "Vegui": { matches: 8, mvp: 1, wins: 1, goalFech: 1, foto: "Vegui.jpg", bonus: 0 },
-  "Mosky": { matches: 4, mvp: 0, wins: 0, goalFech: 0, foto: "Mosky.jpg", bonus: 0 },
-  "Bylu": { matches: 6, mvp: 1, wins: 2, goalFech: 0, foto: "Bylu.jpg", bonus: 0 },
-  "Pana Hija": { matches: 2, mvp: 0, wins: 0, goalFech: 0, foto: "Pana.jpg", bonus: 0 }, 
-  "DJ Manzon": { matches: 6, mvp: 0, wins: 1, goalFech: 0, foto: "manzon.jpg", bonus: 0 }, 
-  "Nuno": { matches: 9, mvp: 0, wins: 2, goalFech: 0, foto: "nuno.jpg", bonus: 0 },
-  "Peter": { matches: 5, mvp: 1, wins: 2, goalFech: 0, foto: "peter.jpg", bonus: 20 },
+  "Chinche": { matches: 10, mvp: 1, wins: 4, goalFech: 1, foto: "Chinche.jpg", bonus: 0 },
+  "Culebra": { matches: 4, mvp: 1, wins: 1, goalFech: 1, foto: "Culebra.jpg", bonus: 0 },
+  "Vegui": { matches: 9, mvp: 1, wins: 1, goalFech: 1, foto: "Vegui.jpg", bonus: 0 },
+  "Mosky": { matches: 5, mvp: 0, wins: 1, goalFech: 0, foto: "Mosky.jpg", bonus: 0 },
+  "Bylu": { matches: 7, mvp: 1, wins: 2, goalFech: 0, foto: "Bylu.jpg", bonus: 0 },
+  "Pana Hija": { matches: 3, mvp: 0, wins: 0, goalFech: 0, foto: "Pana.jpg", bonus: 0 }, 
+  "DJ Manzon": { matches: 7, mvp: 0, wins: 2, goalFech: 0, foto: "manzon.jpg", bonus: 0 }, 
+  "Nuno": { matches: 10, mvp: 0, wins: 3, goalFech: 0, foto: "nuno.jpg", bonus: 0 },
+  "Peter": { matches: 6, mvp: 1, wins: 3, goalFech: 0, foto: "peter.jpg", bonus: 20 },
+  "Tincho": { matches: 5, mvp: 0, wins: 0, goalFech: 0, foto: "ticnho.jpg", bonus: 0 },
 };
 
 const MATCH_HISTORY = [
@@ -39,6 +40,7 @@ const MATCH_HISTORY = [
   { fecha: "10 Feb", resultado: "Pechera 14 - 7 Sin pechera", mvp: "Chinche" },
   { fecha: "17 Feb", resultado: "Pechera 5 - 7 Sin pechera", mvp: "Culebra" },
   { fecha: "22 Feb", resultado: "Pechera 6 - 5 Sin pechera", mvp: "Bylu" },
-  { fecha: "22 Feb", resultado: "Pechera 7 - 4 Sin pechera", mvp: "Vegui" },
-  { fecha: "22 Feb", resultado: "Pechera 12 - 7 Sin pechera", mvp: "???" },
+  { fecha: "08 Feb", resultado: "Pechera 7 - 4 Sin pechera", mvp: "Vegui" },
+  { fecha: "15 Marzo", resultado: "Pechera 12 - 7 Sin pechera", mvp: "Faculo Airbag" },
+  { fecha: "22 Marzo", resultado: "Pechera 2 - 6 Sin pechera", mvp: "???" },
 ];
